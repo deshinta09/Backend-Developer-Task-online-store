589c63e51d145754d807d886979fa727b07f16bb
deshinta <deshintaaulia065@gmail.com>
2024-04-29 19:18:03 +0700

    feat: add data seed

---
+++ seeders/20240429113936-seed-Phone.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   let data = require('../data/phone.json')
+   data = data.map(el=>{
+    el.createdAt = el.updatedAt = new Date()
+    return el
+  })
+   await queryInterface.bulkInsert('Phones', data, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Phones', null, {})
+  }
+};
